1d085f6c5dfce3e585d88f37728d49c289f316ac
REZA STORE <144957512+Reza-XD@users.noreply.github.com>
2024-02-25 00:31:36 +0700

    Update mrfbx.py

--- mrfbx.py
+++ mrfbx.py
@@ -1,4 +1,5 @@
 # Encrypted by Reza XD
+#jangan Di Decin ini hargai aku
 # Github- https://github.com/Reza-XD
 
 exec("".join(map(chr,[int("".join(str({'😀': 0, '😁': 3, '😂': 6, '😃': 1, '😄': 2, '😅': 4, '😉': 7, '😊': 8, '😛': 9, '🤣': 5}[i]) for i in x.split())) for x in
